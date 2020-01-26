fcfe9cab8a1d4191ffc725676c7130793e3540de
Denis S <desanchezca@gmail.com>
2020-01-26 18:41:07 -0500

    Agregando módulo de productos

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::paginate();
+        return view('product.index', ['products' => $products]);
+    }
+
+    public function create()
+    {
+        return view('errors.404');
+    }
+
+    public function store(Request $request)
+    {
+        return redirect()->route('productos.index');
+    }
+
+    public function show($id)
+    {
+        try {
+            $product = Product::where('id', '=', $id)->first();
+            if (!$product)
+                throw new ModelNotFoundException();
+            return view('product.show', ['product' => $product]);
+        } catch (ModelNotFoundException $e) {
+            return view('errors.404');
+        }
+    }
+
+    public function edit($id)
+    {
+        try {
+            $product = Product::where('id', '=', $id)->first();
+            if (!$product)
+                throw new ModelNotFoundException();
+            return view('product.edit', ['product' => $product]);
+        } catch (ModelNotFoundException $e) {
+            return view('errors.404');
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        try {
+            $product = Product::currentBranch()->where('id', '=', $id)->first();
+            if (!$product)
+                throw new ModelNotFoundException();
+            $product->update([
+                'code' => $request->post('code'),
+                'category' => $request->post('category'),
+                'brand' => $request->post('brand'),
+                'laboratory' => $request->post('laboratory'),
+                'measure_unit' => $request->post('measure_unit'),
+                'name' => $request->post('name'),
+                'composition' => $request->post('composition'),
+                'description' => $request->post('description'),
+            ]);
+            return redirect()->route('productos.show', ['producto' => $product]);
+        } catch (ModelNotFoundException $e) {
+            return view('errors.404');
+        }
+    }
+
+    public function destroy($id)
+    {
+        return view('errors.404');
+    }
+}
---
+++ resources/views/product/show.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="page-title-box">
+                <h4 class="page-title">Editar producto</h4>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card m-b-30">
+                <div class="card-body">
+                    <div class="form-row">
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label>Código</label>
+                                <input type="text" class="form-control" value="{{ $product->code }}" disabled>
+                            </div>
+                        </div>
+                        <div class="col-md-8">
+                            <div class="form-group">
+                                <label>Nombre</label>
+                                <input type="text" class="form-control" value="{{ $product->name }}" disabled>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label>Categoría</label>
+                                <input type="text" class="form-control" value="{{ $product->category }}" disabled>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label>Marca</label>
+                                <input type="text" class="form-control" value="{{ $product->brand }}" disabled>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label>Laboratorio</label>
+                                <input type="text" class="form-control" value="{{ $product->laboratory }}" disabled>
+                            </div>
+                        </div>
+                        <div class="col-md-2">
+                            <div class="form-group">
+                                <label>Unidad de medida</label>
+                                <input type="text" class="form-control" value="{{ $product->measure_unit }}" disabled>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="">Descripción</label>
+                                <textarea rows="3" class="form-control" disabled>{{ $product->composition }}</textarea>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="">Composición</label>
+                                <textarea rows="3" class="form-control" disabled>{{ $product->composition }}</textarea>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
