2bab653861842d5fc4477b72d0b0072fd658e706
Denis S <desanchezca@gmail.com>
2020-01-26 11:54:53 -0500

    Agregando plantilla

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 require('./bootstrap');
+
+require('./js/modernizr.min');
+require('./js/detect');
+require('./js/fastclick');
+require('./js/jquery.slimscroll');
+require('./js/jquery.blockUI');
+require('./js/waves');
+require('./js/jquery.nicescroll');
+require('./js/jquery.scrollTo.min');
+require('./js/app');
