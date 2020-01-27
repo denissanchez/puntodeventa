52d6fe1c8c58030d60079f7220176ffe3b754567
Denis S <desanchezca@gmail.com>
2020-01-26 22:47:09 -0500

    Agregando accesors y mutators

--- app/Models/Branch.php
+++ app/Models/Branch.php
@@ -25,6 +25,26 @@ class Branch extends Model
         return $query->where('id', '=', Auth::user()->branch_id);
     }
 
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = strtoupper($value);
+    }
+
+    public function setAddressAttribute($value)
+    {
+        $this->attributes['address'] = strtoupper($value);
+    }
+
+    public function setEmailAttribute($value)
+    {
+        $this->attributes['email'] = strtolower($value);
+    }
+
+    public function setWebsiteAttribute($value)
+    {
+        $this->attributes['website'] = strtolower($value);
+    }
+
     public function users()
     {
         return $this->hasMany(User::class);
