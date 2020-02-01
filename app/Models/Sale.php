0e374261a6266532e6fe1b754584d9df9dabe62d
Denis S <desanchezca@gmail.com>
2020-02-01 10:15:55 -0500

    Agregando modelo y migraciones

--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Sale extends Model
 {
-    //
+    protected $fillable = [
+        'branch_id', 'seller_id', 'code', 'owner_document',
+        'state', 'type', 'currrency', 'commentary'
+    ];
+
+    public function setOwnerDocumentAttribute($value: array)
+    {
+        $this->attributes['owner_document'] = serialize($value);
+    }
+
+    public function getOwnerDocumentAttribute()
+    {
+        return unserialize($this->attributes['owner_document']);
+    }
 }
---
+++ database/migrations/2020_02_01_035104_create_sale_details_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSaleDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sale_details', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('sale_id');
+            $table->string('product_id');
+            $table->string('sale_code');
+            $table->string('item');
+            $table->string('purchase_code');
+            $table->decimal('quantity', 8, 2);
+            $table->decimal('unit_price', 8, 2);
+            $table->decimal('discount', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sale_details');
+    }
+}
