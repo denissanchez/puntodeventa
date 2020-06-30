cdc7c769e21607b14de363ee19f6a231adf551c2
Denis Sanchez <desanchezca@gmail.com>
2020-06-30 16:42:37 -0500

    Agregando Seeder a la tabla usuarios

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Denis Sanchez',
+            'username' => 'desanchez',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+
+        User::create([
+            'name' => 'Edison Chavez',
+            'username' => 'echavez',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+
+        User::create([
+            'name' => 'Juan Bardales',
+            'username' => 'jbardales',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+    }
+}
